d50ae24f7fe8a7ce1b13e04e1caa5f07e7c197b3
mesay50 <mesayy2017@gmail.com>
2019-08-14 16:05:57 -0400

    first commit

---
+++ src/computer/HardWare.java
@@ -0,0 +1,9 @@
+package computer;
+
+public class HardWare {
+	public static void main(String[] args) {
+		int age =21;
+		System.out.println(age);
+	}
+
+}
---
+++ src/computer/SoftWare.java
@@ -0,0 +1,9 @@
+package computer;
+
+public class SoftWare {
+	public static void main(String[] args) {
+		int age1 =21;
+		System.out.println(age1);
+	}
+
+}
